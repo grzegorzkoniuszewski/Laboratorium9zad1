7b4ac7c804a4388c93e7fe8004f9a315ba6c97e2
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2025-01-11 10:57:01 +0100

    Add NullPointerExceptionGenerator.java to demonstrate NullPointerException handling

---
+++ src/NullPointerExceptionGenerator.java
@@ -0,0 +1,6 @@
+public class NullPointerExceptionGenerator {
+    public static void generateNullPointerException() {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
